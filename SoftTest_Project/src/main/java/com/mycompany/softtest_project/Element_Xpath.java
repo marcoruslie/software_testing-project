d6963699194a1fb3b9dedebc9b17507ef1c4ee81
andrewloekito123 <82990119+andrewloekito123@users.noreply.github.com>
2023-06-16 01:04:38 +0700

    Progress ( Add Item )

--- SoftTest_Project/src/main/java/com/mycompany/softtest_project/Element_Xpath.java
+++ SoftTest_Project/src/main/java/com/mycompany/softtest_project/Element_Xpath.java
@@ -31,4 +31,30 @@ public class Element_Xpath {
     private final String loginBtnHomeXpath = "/html/body/header/div/div/div[3]/div[1]/div/a";
     private final String loginBtnXpath = "/html/body/section[2]/div/div/div/div/form/div[4]/button";
 
+    // Website Builder
+
+    // Select Theme
+    private final String settings = "/html/body/div[1]/div[2]/div/div[1]/div/ul/li[8]/a";
+    private final String themes = "/html/body/div[1]/div[2]/div/div[1]/div/ul/li[8]/div/ul/li[1]/a";
+    private final String selectTheme = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div/div/div[8]/label/figure/img";
+    private final String selectThemeButton = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[3]/div/div/button";
+
+    // Hero Section
+    private final String HomeMenu = "/html/body/div[1]/div[2]/div/div[1]/div/ul/li[9]/a";
+    private final String HeroSection = "/html/body/div[1]/div[2]/div/div[1]/div/ul/li[9]/div/ul/li[1]/a";
+    private final String selectLanguage = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[1]/div/div[2]/select";
+    private final String addSlider = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[1]/div/div[3]/a";
+
+    // Modify Hero Section
+    private final String heroImgUrl = "C:\\Users\\andre\\Desktop\\Ga Penting\\coki-pardede_169.jpeg";
+    private final String heroImgUrl2 = "C:\\Users\\andre\\Desktop\\Ga Penting\\WhatsApp Image 2023-06-15 at 22.48.35.jpeg";
+    private final String selectHeroLanguage ="/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/select";
+    private final String chooseHeroFile = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[2]/input";
+    private final String heroTitle = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[3]/div[1]/div/input";
+    private final String heroSubtitle = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[3]/div[2]/div/input";
+    private final String btHeroName = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[4]/div[1]/div/input";
+    private final String btHeroUrl = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[4]/div[2]/div/input";
+    private final String heroSerial = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[5]/div/div/input";
+    private final String heroSaveBtn = "/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[3]/div/div/button";
+
 }
--- SoftTest_Project/src/main/java/com/mycompany/softtest_project/SoftTest_Project.java
+++ SoftTest_Project/src/main/java/com/mycompany/softtest_project/SoftTest_Project.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.softtest_project;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.InputEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -56,6 +59,313 @@ public class SoftTest_Project {
 //        registerUser();
 //        billingDetailUser();
         loginUser();
+        // Web Templating
+        WebElement settings = driver.findElement(By.xpath(eXpath.getSettings()));
+        settings.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        WebElement themes = driver.findElement(By.xpath(eXpath.getThemes()));
+        themes.click();
+        WebElement selectTheme = driver.findElement(By.xpath(eXpath.getSelectTheme()));
+        selectTheme.click();
+        WebElement saveTheme = driver.findElement(By.xpath(eXpath.getSelectThemeButton()));
+        saveTheme.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        WebElement HomeSection = driver.findElement(By.xpath(eXpath.getHomeMenu()));
+        HomeSection.click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        // Hero Section
+//        WebElement HomeSectionHero = driver.findElement(By.xpath(eXpath.getHeroSection()));
+//        HomeSectionHero.click();
+//        WebElement HomeSectionHeroLanguage = driver.findElement(By.xpath(eXpath.getSelectLanguage()));
+//        HomeSectionHeroLanguage.click();
+//        HomeSectionHeroLanguage.sendKeys("English");
+//        WebElement AddSliderBtn = driver.findElement(By.xpath(eXpath.getAddSlider()));
+//        AddSliderBtn.click();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//
+//        }
+//        WebElement heroLanguage = driver.findElement(By.xpath(eXpath.getSelectHeroLanguage()));
+//        heroLanguage.click();
+//        heroLanguage.sendKeys("English");
+//        WebElement heroImageFile = driver.findElement(By.xpath(eXpath.getChooseHeroFile()));
+//        heroImageFile.sendKeys(eXpath.getHeroImgUrl());
+//        WebElement heroTitle = driver.findElement(By.xpath(eXpath.getHeroTitle()));
+//        heroTitle.sendKeys("Test");
+//        WebElement heroSubtitle = driver.findElement(By.xpath(eXpath.getHeroSubtitle()));
+//        heroSubtitle.sendKeys("Test");
+//        WebElement heroButton = driver.findElement(By.xpath(eXpath.getBtHeroName()));
+//        heroButton.sendKeys("Test");
+//        WebElement heroButtonLink = driver.findElement(By.xpath(eXpath.getBtHeroUrl()));
+//        heroButtonLink.sendKeys("https://www.google.com/");
+//        WebElement heroSerialNumber = driver.findElement(By.xpath(eXpath.getHeroSerial()));
+//        heroSerialNumber.sendKeys("1");
+//        WebElement heroSave = driver.findElement(By.xpath(eXpath.getHeroSaveBtn()));
+//        heroSave.click();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//
+//        }
+//        WebElement heroLanguage2 = driver.findElement(By.xpath(eXpath.getSelectHeroLanguage()));
+//        heroLanguage2.click();
+//        heroLanguage2.sendKeys("English");
+//        WebElement heroImageFile2 = driver.findElement(By.xpath(eXpath.getChooseHeroFile()));
+//        heroImageFile2.sendKeys(eXpath.getHeroImgUrl2());
+//        WebElement heroTitle2 = driver.findElement(By.xpath(eXpath.getHeroTitle()));
+//        heroTitle2.sendKeys("Test");
+//        WebElement heroSubtitle2 = driver.findElement(By.xpath(eXpath.getHeroSubtitle()));
+//        heroSubtitle2.sendKeys("Test");
+//        WebElement heroButton2 = driver.findElement(By.xpath(eXpath.getBtHeroName()));
+//        heroButton2.sendKeys("Test");
+//        WebElement heroButtonLink2 = driver.findElement(By.xpath(eXpath.getBtHeroUrl()));
+//        heroButtonLink2.sendKeys("https://www.google.com/");
+//        WebElement heroSerialNumber2 = driver.findElement(By.xpath(eXpath.getHeroSerial()));
+//        heroSerialNumber2.sendKeys("2");
+//        WebElement heroSave2 = driver.findElement(By.xpath(eXpath.getHeroSaveBtn()));
+//        heroSave2.click();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+//        // Home Section
+//        WebElement HomeSectionHome = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[9]/div/ul/li[2]/a"));
+//        HomeSectionHome.click();
+//        WebElement FeatureItemTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/div[1]/div[2]/div/div/input[2]"));
+//        FeatureItemTitle.sendKeys("Feature Item Title");
+//        WebElement NewItemTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/div[2]/div[2]/div/div/input[2]"));
+//        NewItemTitle.sendKeys("New Item Title");
+//        WebElement BestSellerTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/div[3]/div[2]/div/div/input[2]"));
+//        BestSellerTitle.sendKeys("Best Seller Title");
+//        WebElement TopRatedTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/div[4]/div[2]/div/div/input[2]"));
+//        TopRatedTitle.sendKeys("Top Rated Title");
+//        WebElement SpecialItemTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/div[5]/div[2]/div/div/input[2]"));
+//        SpecialItemTitle.sendKeys("Special Item Title");
+//        WebElement FlashSaleItemTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/div[6]/div[2]/div/div/input[2]"));
+//        FlashSaleItemTitle.sendKeys("Flash Sale Item Title");
+//        WebElement NewsletterTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[2]/div/div[2]/div[1]/div/input[2]"));
+//        NewsletterTitle.sendKeys("Newsletter Title");
+//        WebElement NewsletterSubtitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[2]/div/div[2]/div[2]/div/input[2]"));
+//        NewsletterSubtitle.sendKeys("Newsletter Subtitle");
+//        WebElement HomeSectionHomeBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[3]/div/div/div/button"));
+//        HomeSectionHomeBtn.click();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//
+//        }
+        // Features
+//        WebElement Features = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[9]/div/ul/li[3]/a"));
+//        Features.click();
+//        WebElement addFeatureBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[1]/div/div[3]/a"));
+//        addFeatureBtn.click();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//        }
+//        WebElement featureLanguage = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[1]/select"));
+//        featureLanguage.click();
+//        featureLanguage.sendKeys("English");
+//        WebElement featureIcon = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[2]/input"));
+//        featureIcon.sendKeys("C:\\Users\\andre\\Desktop\\Ga Penting\\Untitled.png");
+//        WebElement featureTitle = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[3]/input"));
+//        featureTitle.sendKeys("Feature Title");
+//        WebElement featureText = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[4]/textarea"));
+//        featureText.sendKeys("Feature Text");
+//        WebElement featureSerial = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[5]/input"));
+//        featureSerial.sendKeys("1");
+//        WebElement featureSaveBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[3]/button[2]"));
+//        featureSaveBtn.click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        // Offer Banner
+//        WebElement OfferBanner = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[9]/div/ul/li[4]/a"));
+//        OfferBanner.click();
+//        WebElement addOfferBannerBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[1]/div/div[3]/a"));
+//        addOfferBannerBtn.click();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//        }
+//        WebElement offerBannerLanguage = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[1]/select"));
+//        offerBannerLanguage.click();
+//        offerBannerLanguage.sendKeys("English");
+//        WebElement offerBannerImage = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[2]/input"));
+//        offerBannerImage.sendKeys("C:\\Users\\andre\\Desktop\\Ga Penting\\Screenshot 2021-12-21 200207.png");
+//        WebElement offerBannerText1 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[3]/input"));
+//        offerBannerText1.sendKeys("Offer Banner Text 1");
+//        WebElement offerBannerText2 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[4]/input"));
+//        offerBannerText2.sendKeys("Offer Banner Text 2");
+//        WebElement offerBannerText3 = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[5]/input"));
+//        offerBannerText3.sendKeys("Offer Banner Text 3");
+//        WebElement offerBannerUrl = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[6]/input"));
+//        offerBannerUrl.sendKeys("https://www.google.com/");
+//        WebElement offerBannerPosition = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[7]/select"));
+//        offerBannerPosition.click();
+//        offerBannerPosition.sendKeys("Top");
+//        WebElement offerBannerSaveBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[3]/button[2]"));
+//        offerBannerSaveBtn.click();
+
+        // Footer
+//        WebElement Footer = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[10]/a"));
+//        Footer.click();
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//        }
+//        WebElement FooterSectionClick = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[10]/div/ul/li[1]/a"));
+//        FooterSectionClick.click();
+//        WebElement addFooterLogo = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[1]/input"));
+//        addFooterLogo.sendKeys("C:\\Users\\andre\\Desktop\\Ga Penting\\Orang-tua-new.jpg");
+//        WebElement footerAboutCompany = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[2]/textarea"));
+//        footerAboutCompany.sendKeys("At InnovateTech Solutions, our mission is to empower businesses with technology-driven solutions that drive growth, efficiency, and success. We are committed to providing top-notch software solutions tailored to meet the unique needs of each client, enabling them to achieve their goals and outperform their competition.");
+//        WebElement copyrightFooter = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/form/div[3]/div/div[3]/div[2]/p"));
+//        copyrightFooter.sendKeys("Copyright");
+//        WebElement footerSaveBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[3]/div/div/button"));
+//        footerSaveBtn.click();
+
+        // ShopManagement
+        WebElement ShopManagement = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[5]/a"));
+        ShopManagement.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        WebElement ManageItemsClick = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[5]/div/ul/li[4]/a"));
+        ManageItemsClick.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        WebElement categorySection = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[5]/div/ul/li[4]/div/ul/li[1]/a"));
+        categorySection.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+//        for (int i = 1; i < 6; i++) {
+//            addCat(i);
+//        }
+        WebElement subCategorySection = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[5]/div/ul/li[4]/div/ul/li[2]/a"));
+        subCategorySection.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+//        WebElement addSubCategoryBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[1]/div/div[3]/a"));
+//        addSubCategoryBtn.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+//        for (int i = 1; i < 6; i++) {
+//            addSubCat(i);
+//        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        WebElement addItem = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div/ul/li[5]/div/ul/li[4]/div/ul/li[3]/a"));
+        addItem.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        WebElement addItemBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div[1]/a/div/div"));
+        addItemBtn.click();
+        addItem();
+
+    }
+    static void addItem(){
+        WebElement targetDropArea = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[2]/div/div/div[2]/form"));
+        String filePath = "C:\\Users\\andre\\Desktop\\Ga Penting\\130fe332c7dd9b35019c6fcea6426ed0.jpg";
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(filePath), null);
+        try {
+            Robot robot = new Robot();
+            robot.setAutoDelay(2000);
+            robot.mouseMove(targetDropArea.getLocation().getX()+200, targetDropArea.getLocation().getY()+150);
+            robot.setAutoDelay(1000);
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            robot.keyPress(java.awt.event.KeyEvent.VK_CONTROL);
+            robot.keyPress(java.awt.event.KeyEvent.VK_V);
+            robot.keyRelease(java.awt.event.KeyEvent.VK_V);
+            robot.keyRelease(java.awt.event.KeyEvent.VK_CONTROL);
+            robot.keyPress(java.awt.event.KeyEvent.VK_ENTER);
+            robot.keyRelease(java.awt.event.KeyEvent.VK_ENTER);
+        }catch (AWTException e){
+            e.printStackTrace();
+        }
+    }
+    static void addSubCat(int index){
+        WebElement subCategoryLanguage = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[1]/select"));
+        subCategoryLanguage.click();
+        subCategoryLanguage.sendKeys("English");
+        WebElement subCategoryCat = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[2]/select"));
+        subCategoryCat.click();
+        subCategoryCat.sendKeys("Category "+index);
+        WebElement subCategoryName = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[3]/input"));
+        subCategoryName.sendKeys("Sub Category "+index);
+        WebElement subCategoryStatus = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[4]/select"));
+        subCategoryStatus.click();
+        subCategoryStatus.sendKeys("Active");
+        WebElement subCategorySaveBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[3]/button[2]"));
+        subCategorySaveBtn.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+    static void addCat(int index){
+        WebElement addCategoryBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[2]/div/div/div[1]/div/div[3]/a"));
+        addCategoryBtn.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+        WebElement categoryLanguage = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[1]/select"));
+        categoryLanguage.click();
+        categoryLanguage.sendKeys("English");
+        WebElement categoryImage = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[2]/input"));
+        categoryImage.sendKeys("C:\\Users\\andre\\Desktop\\Ga Penting\\507284.jpg");
+        WebElement categoryName = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[3]/input"));
+        categoryName.sendKeys("Category " + index);
+        WebElement catStatus = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[2]/form/div[4]/select"));
+        catStatus.click();
+        catStatus.sendKeys("Active");
+        WebElement catSaveBtn = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[3]/div/div/div[3]/button[2]"));
+        catSaveBtn.click();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
     }
     private static User ReadCSV() {
         String[] values={};
